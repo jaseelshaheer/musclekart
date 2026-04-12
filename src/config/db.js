88c686811eabe7474feede19dab2a54bc260417a
jaseelshaheer <iamjaseel98@gmail.com>
2026-04-12 23:48:31 +0530

    configuration change for mongodb uri

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,14 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/musclekart");
+  // await mongoose.connect("mongodb://127.0.0.1:27017/musclekart");
+  const mongoUri = process.env.MONGODB_URI;
+
+  if (!mongoUri) {
+    throw new Error("MONGODB_URI is not set");
+  }
+
+  await mongoose.connect(mongoUri);
   console.log("MongoDb connected");
 };
 
